924b20e456a553be87bd9ccaf54cd3bb817b9dd6
geemarkwell <glennsom2003@gmail.com>
2024-09-15 03:26:37 +0100

    image replace

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ export const metadata: Metadata = {
   description:
     'No more endless scrolling or waiting for packages to arrive',
   openGraph: {
-    images: '/opengraph-image.png'
+    images: ''
   },
   twitter: {
     card: 'summary_large_image',
